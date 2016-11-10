7a87c21bc48a5b4d2a511bc68c80f40b477bea0f
Zarete <thomas@Zarete>
2016-11-10 20:45:59 +0100

    update

---
+++ Functions.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    GREEN = '\033[92m'
+    xxx = '\033[93m'
+    RED = '\033[91m'
+    STOP = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
--- moriarty.py
+++ moriarty.py
@@ -1,5 +1,41 @@
 # -*- coding: utf-8 -*-
 
+import Data
+import Functions
+
 grille = [(x, y) for x in range(8) for y in range(8)]
 
-print(len(grille))
+def players():
+	"""Players attribution
+
+	Ask to each player is pseudo and attribute him/her
+	a role in the game
+
+	Args : 
+		None
+	Return :
+		return a dictionnary containing the pseudo and role
+		of each players"""
+
+	#TODO : Ajout d'une BDD des différents joueurs avec des scores et vérifier la présence des joueurs choisis dans cette BDD pour charger les scores
+
+	activator = ''
+	inhibitor = ''
+
+	activator = input("Entrez le pseudo du joueur 'Activator' : ")
+	inhibitor = input("Entrez le pseudo du joueur 'Inhibitor' : ")
+
+	return activator, inhibitor
+	
+
+print("Bienvenue dans ce jeu de Moriarty !")
+activator, inhibitor = players()
+
+print('\nFor this game :'
+
+	'\n[+]', activator, 'is the '+Functions.bcolors.RED+'Activator'+Functions.bcolors.STOP+
+	
+	'\n[+]', inhibitor, 'is the '+Functions.bcolors.GREEN+'Inhibitor\n'+Functions.bcolors.STOP)
+
+
+
