e700a32542f25d16bb29f4d490da427200c13c7c
Zarete <thomas@Zarete>
2016-11-11 01:27:43 +0100

    update

--- Functions.py
+++ Functions.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
-class bcolors:
+import Data
+
+# Add colors into the terminal
+class colors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
     GREEN = '\033[92m'
@@ -9,3 +12,46 @@ class bcolors:
     STOP = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
+
+def players():
+	"""Players attribution
+
+	Ask to each player is pseudo and attribute him/her
+	a role in the game
+
+	Args : 
+		None
+	Return :
+		return a dictionnary containing the pseudo and role
+		of each players"""
+
+	#TODO : Ajout d'une BDD des différents joueurs avec des scores et vérifier la présence des joueurs choisis dans cette BDD pour charger les scores
+
+	activator = ''
+	inhibitor = ''
+
+	activator = input(colors.GREEN+"Entrez le pseudo du joueur 'Activator' : "+colors.STOP)
+	inhibitor = input(colors.RED+"Entrez le pseudo du joueur 'Inhibitor' : "+colors.STOP)
+
+	return activator, inhibitor
+
+
+def display_grid(grid):
+	"""Display grid
+
+	Display the updated grid
+
+	Args:
+		grid : current grid to display
+	Return:
+		None"""
+
+	for i in range(64):
+		for value in grid[i].values():
+			if i % 8 == 0:
+				print('\n\n' + str(value), end = ' ')
+
+			else:
+				print(str(value), end = ' ')
+
+	print('\n')
--- moriarty.py
+++ moriarty.py
@@ -3,39 +3,16 @@
 import Data
 import Functions
 
-grille = [(x, y) for x in range(8) for y in range(8)]
-
-def players():
-	"""Players attribution
-
-	Ask to each player is pseudo and attribute him/her
-	a role in the game
-
-	Args : 
-		None
-	Return :
-		return a dictionnary containing the pseudo and role
-		of each players"""
-
-	#TODO : Ajout d'une BDD des différents joueurs avec des scores et vérifier la présence des joueurs choisis dans cette BDD pour charger les scores
-
-	activator = ''
-	inhibitor = ''
-
-	activator = input("Entrez le pseudo du joueur 'Activator' : ")
-	inhibitor = input("Entrez le pseudo du joueur 'Inhibitor' : ")
-
-	return activator, inhibitor
-	
+grid = [{(x,y) : ' * ' } for y in range(8) for x in range(8)]
 
 print("Bienvenue dans ce jeu de Moriarty !")
-activator, inhibitor = players()
+
+activator, inhibitor = Functions.players()
 
 print('\nFor this game :'
 
-	'\n[+]', activator, 'is the '+Functions.bcolors.RED+'Activator'+Functions.bcolors.STOP+
+	'\n[+]', activator, 'is the '+Functions.colors.RED+'Activator'+Functions.colors.STOP+
 	
-	'\n[+]', inhibitor, 'is the '+Functions.bcolors.GREEN+'Inhibitor\n'+Functions.bcolors.STOP)
+	'\n[+]', inhibitor, 'is the '+Functions.colors.GREEN+'Inhibitor\n'+Functions.colors.STOP)
 
-
-
+Functions.display_grid(grid)
