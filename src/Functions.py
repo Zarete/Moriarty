f3cb1d42dda2d51d40bb81c50bc5964ad93010eb
Zarete <maucourt.t@gmail.com>
2016-11-11 17:58:51 +0100

    Add display

--- src/Functions.py
+++ src/Functions.py
@@ -29,8 +29,17 @@ def player_definition():
 	activator = ''
 	inhibitor = ''
 
-	activator = input(colors.GREEN+"Entrez le pseudo du joueur 'Activator' : "+colors.STOP)
-	inhibitor = input(colors.RED+"Entrez le pseudo du joueur 'Inhibitor' : "+colors.STOP)
+	print("Entrez le pseudo du joueur",colors.GREEN + "'Activator' : " + colors.STOP, end = "")
+	activator = input()
+
+	print("Entrez le pseudo du joueur", colors.RED + "'Inhibitor' : "+colors.STOP, end = "")
+	inhibitor = input()
+
+	if len(activator) == 0:
+		activator = 'Activator'
+
+	if len(inhibitor) == 0:
+		inhibitor = 'Inhibitor'
 
 	return activator, inhibitor
 
@@ -45,13 +54,23 @@ def display_grid(grid):
 	Return:
 		None"""
 
+	print("""
+            0     1     2     3     4     5     6     7
+	 
+            ▼     ▼     ▼     ▼     ▼     ▼     ▼     ▼   """, colors.BOLD + "(X)" + colors.STOP)
+
+	row = 0
+
 	for i in range(64):
 		for value in grid[i].values():
 			if i % 8 == 0:
-				print('\n\n' + str(value), end = ' ')
+				print('  \n  \n  ' + str(row) + '   ▶   ' , str(value), end = '   ')
+				row += 1
 
 			else:
-				print(str(value), end = ' ')
+				print(str(value), end = '   ')
+	print(colors.BOLD + '\n\n (Y)' + colors.STOP)
+
 
 	print('\n')
 
