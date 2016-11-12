997e47ec629ea775595d968dabbfb25bfdfdd550
Zarete <maucourt.t@gmail.com>
2016-11-12 23:05:58 +0100

    update

--- Moriarty.py
+++ Moriarty.py
@@ -1,23 +1,38 @@
 # -*- coding: utf-8 -*-
 
+"""CHOIX DE PIONS DE FORMES DIFFERENTES POUR EVITER LES PROBLEMES
+   DE DALTONISME -> CHOIX A FAIRE (METTRE DANS RAPPORT)"""
+
 import src.Data as Data
 import src.Functions as Functions
+from random import choice
+import os
 
-print("Bienvenue dans ce jeu de Moriarty !")
+os.system('clear')
+
+print(Functions.colors.HEADER + "			Bienvenue dans ce jeu de Moriarty !" + Functions.colors.STOP)
 
 grid = Functions.create_initial_grid()
 
-Functions.create_player()
+activator, inhibitor = Functions.create_player()
+
+player = choice([activator, inhibitor])
 
 print('\nFor this game :'
 
-	'\n 	[+]', Functions.colors.GREEN + Data.current_player['Activator'] + Functions.colors.STOP, 'is the '+Functions.colors.GREEN+'Activator'+Functions.colors.STOP+
+	'\n 	[+]', Functions.colors.GREEN + activator + Functions.colors.STOP, 'is the '+Functions.colors.GREEN+'Activator'+Functions.colors.STOP+
 	
-	'\n 	[+]', Functions.colors.RED + Data.current_player['Inhibitor'] + Functions.colors.STOP, 'is the '+Functions.colors.RED+'Inhibitor\n'+Functions.colors.STOP)
+	'\n 	[+]', Functions.colors.RED + inhibitor + Functions.colors.STOP, 'is the '+Functions.colors.RED+'Inhibitor\n'+Functions.colors.STOP)
 
+while True:
 
-Functions.display_grid(grid)
+	#os.system('clear')
 
-grid = Functions.playing(Data.current_player['Activator'], grid)
+	Functions.display_grid(grid)
 
-Functions.display_grid(grid)
+	grid = Functions.playing(player, grid)
+
+	if player == activator:
+		player = Data.current_player['Inhibitor']
+	else:
+		player = Data.current_player['Activator']
--- src/Functions.py
+++ src/Functions.py
@@ -1,17 +1,22 @@
 # -*- coding: utf-8 -*-
 
 from . import Data
+import os
 
 # Add colors into the terminal
 class colors:
-    GREENB = '\033[42m'
-    GREEN = '\033[92m'
+	
+	HEADER = '\033[95m'
 
-    REDB = '\033[41m'
-    RED = '\033[91m'
+	GREENB = '\033[42m'
+	GREEN = '\033[92m'
 
-    STOP = '\033[0m'
-    BOLD = '\033[1m'
+	REDB = '\033[41m'
+	RED = '\033[91m'
+
+	STOP = '\033[0m'
+	BOLD = '\033[1m'
+
 
 def create_initial_grid():
 	"""Create the grid
@@ -25,14 +30,16 @@ def create_initial_grid():
 		return a list of dictionnaries containing coordinates and status of
 	"""
 
-	grid = [{(x,y) : ' + ' } for y in range(8) for x in range(8)]
+	grid = {(x,y) : ' + ' for x in range(8) for y in range(8)}
 
-	grid[27][(3,3)] = colors.REDB + "   " + colors.STOP
-	grid[28][(4,3)] = colors.GREENB + "   " + colors.STOP
-	grid[35][(3,4)] = colors.GREENB + "   " + colors.STOP
-	grid[36][(4,4)] = colors.REDB + "   " + colors.STOP
+	# Define initial positions 
+	grid[(3,3)] = colors.REDB + "   " + colors.STOP
+	grid[(4,3)] = colors.GREENB + "   " + colors.STOP
+	grid[(3,4)] = colors.GREENB + "   " + colors.STOP
+	grid[(4,4)] = colors.REDB + "   " + colors.STOP
 
 	return grid
+
 
 def create_player():
 	"""Players attribution
@@ -51,10 +58,10 @@ def create_player():
 	activator = ''
 	inhibitor = ''
 
-	print("Entrez le pseudo du joueur",colors.GREEN + "'Activator' : " + colors.STOP, end = "")
+	print("\nEntrez le pseudo du joueur",colors.GREEN + "'Activator' : " + colors.STOP, end = "")
 	activator = input()
 
-	print("Entrez le pseudo du joueur", colors.RED + "'Inhibitor' : "+colors.STOP, end = "")
+	print("\nEntrez le pseudo du joueur", colors.RED + "'Inhibitor' : "+colors.STOP, end = "")
 	inhibitor = input()
 
 	if len(activator) == 0:
@@ -63,8 +70,11 @@ def create_player():
 	if len(inhibitor) == 0:
 		inhibitor = 'Inhibitor'
 
+	# Attribute to each player the status he chose
 	Data.current_player['Activator'] = activator
 	Data.current_player['Inhibitor'] = inhibitor
+
+	return activator, inhibitor
 
 
 def display_grid(grid):
@@ -82,20 +92,187 @@ def display_grid(grid):
 	 
             ▼     ▼     ▼     ▼     ▼     ▼     ▼     ▼   """, colors.BOLD + "(X)" + colors.STOP, end = '')
 
+	print('\n\n')
+
 	row = 0
 
-	for i in range(64):
-		for value in grid[i].values():
-			if i % 8 == 0:
-				print('\n\n\n  ' + str(row) + '   ▶   ' , str(value), end = '   ')
-				row += 1
+	for i in range(8):
+		print(' ', row, '  ▶ ', end = '   ')
+		for j in range(8):
+			print(grid[j,i], end = '   ')
+		print('\n\n')
+		row += 1
+
+	print(colors.BOLD + ' (Y)\n' + colors.STOP)
+
+
+def check_changes(coordXY, case, no_case, grid):
+	"""Check changes induced
+
+	Check if the given coordinates induce changement of state
+	into the grid. 
+
+	Args :
+		coordXY : tuple containing the coordinates (X,Y) chosen by the player
+
+	Return :
+		return True if the coordinates induce a changement else return False"""
+
+	#TODO : Corriger -> erreur sur le droit de positionner les pions
+
+	testing_coord, surrounding_coord = generate_allowed_positions(coordXY, grid)
+
+	#print(surrounding_coord)
+	#print(testing_coord)
+
+	for key, value in testing_coord.items():
+		for cle, valeur in surrounding_coord.items():
+
+			if case in value and no_case in valeur and value != no_case:
+				print('°°', key, '££', cle)
+				grid[key] = case
+				grid[cle] = case
+				return grid, True
+	
+	return grid, False
+
+
+"""
+	for i in range(coordXY[0] - 2, coordXY[0] + 3, 4):
+		for j in range(coordXY[1] - 2, coordXY[1] + 3, 2):
+			try:
+				if grid[i,j] == case and grid(i-1, j-1) not in [no_case, '   ']:
+					print('T1')
+					return True
+			except KeyError:
+				pass
+	try:
+		if grid[(coordXY[0], coordXY[1] - 2)] == case or grid[(coordXY[0], coordXY[1] + 2)] == case and grid[(coordXY[0], coordXY[1] - 1)] not in [no_case, '   '] or grid[(coordXY[0], coordXY[1] + 1)] not in [no_case, '   ']:
+			print('T2')
+			return True
+
+		else:
+			return False
+	except:
+		pass"""
+
+def generate_allowed_positions(coordXY, grid):
+	"""Allowed positions
+
+	Calculate the coordonates allowed for modifications
+
+	Args :
+		coordsXY : coordonates entered by the player
+		grid : current grid
+	Return :
+		testing_coord : coordinates to test
+		surrounging_coord : nearby coordinates of the coordXY spot"""
+
+	surrounding_coord = {}
+	testing_coord = {}
+
+	for i in range(coordXY[0] - 1, coordXY[0]+2, 1):
+		for j in range(coordXY[1] - 1, coordXY[1] +2, 1):
+			try:
+				if i < 0 or j < 0:
+					raise ValueError
+
+				surrounding_coord[(i,j)] = grid[(i,j)]
+
+			except ValueError:
+				pass
+
+			
+
+	for i in range(coordXY[0] - 2, coordXY[0] + 3, 4):
+		for j in range(coordXY[1] - 2, coordXY[1] + 3, 2):
+
+			try:
+				if i < 0 or j < 0 or i > 7 or j > 7:
+					raise ValueError
+
+				testing_coord[(i,j)] = grid[(i,j)]
+
+			except ValueError:
+				pass
+
+	try:
+		coord_tmp1 = (coordXY[0], coordXY[1] + 2)
+
+		if coord_tmp1[1] not in range(8):
+			raise ValueError
+
+		testing_coord[(coordXY[0], coordXY[1] + 2)] = grid[(coordXY[0], coordXY[1] + 2)] 
+
+		coord_tmp2 = (coordXY[0], coordXY[1] - 2)
+
+		if coord_tmp2[1] not in range(8):
+			raise ValueError
+
+		testing_coord[(coordXY[0], coordXY[1] - 2)] = grid[(coordXY[0], coordXY[1] - 2)]
+
+	except ValueError:
+		pass
+
+	return testing_coord, surrounding_coord
+
+def check_position(c_player, case, no_case, grid):
+	"""Check given position
+
+	Check if the player give a position he can use. The player can not use a position
+	if directly next to him there is not an occupied case. He can not use a position
+	if the using of the position does not modify one protein at list. 
+
+	Args :
+		coordXY : the position the user submitted to place his protein
+		case : the case associated with the current player
+		grid : the current grid
+	Return :
+		coordXY : the coordinates chosen by the player"""
+
+	status = False
+
+	# Player coordinates choice
+	while not status:
+		#os.system('clear')
+
+		try:
+			print('\n' + c_player, 'a vous de jouer donnez la coordonnée de X : ', end = '')
+			coordX = int(input())
+
+			print('\n' + c_player, 'a vous de jouer donnez la coordonnée de Y : ', end = '')
+			coordY = int(input())
+
+			if coordX not in range(8) or coordY not in range(8):
+				print('E1')
+				raise ValueError
+
+			if grid[coordX,coordY] != ' + ':
+				print('E2')
+				raise ValueError
+
+			grid, tmp = check_changes((coordX,coordY), case, no_case, grid)
+			
+			if tmp == False:
+				print('E3')
+				raise ValueError
 
 			else:
-				print(str(value), end = '   ')
-	print(colors.BOLD + '\n\n (Y)' + colors.STOP)
+				status = True
 
+		except ValueError:
+			print(""" 
+ Vous ne respectez pas les conditions :
 
-	print('\n')
+ 	[+] Coordonnées dans l'intervalle 0 - 7
+
+ 	[+] Coordonnées doivent induire un changement d'état d'au moins une protéine
+
+ 	[+] Coordonnées ne doivent pas être celles d'une case déjà modifiée
+				""")
+
+	return ((coordX,coordY), grid)
+
 
 def playing(player, grid):
 	"""Play a round
@@ -105,28 +282,28 @@ def playing(player, grid):
 	Args :
 		player : player of the current round
 	Return :
-		coordinates : coordinates of the chosen case as a tuple (X,Y)"""
+		grid : the updated grid"""
 
 	#TODO : Gestion des erreurs sur les coordonnées et sur les cases accessibles
 
+	# Determine the current player and define the colors to use to fill the case of the grid he chose
 	if player == Data.current_player['Activator']:
 		case = colors.GREENB + '   ' + colors.STOP
+		no_case = colors.REDB + '   ' + colors.STOP
+		c_player = colors.GREEN + player + colors.STOP
+		print('Joueur actuel : ' + colors.GREEN + player + colors.STOP)
 
 	else:
 		case = colors.REDB + '   ' + colors.STOP
+		no_case = colors.GREENB + '   ' + colors.STOP
+		c_player = colors.RED + player + colors.STOP
+		print('Joueur actuel : ' + colors.RED + player + colors.STOP)
 
-	print(player, 'a vous de jouer donnez la coordonnée de X : ', end = '')
-	coordX = int(input())
+	coordXY, grid = check_position(c_player, case, no_case, grid)
 
-	print(player, 'a vous de jouer donnez la coordonnée de Y : ', end = '')
-	coordY = int(input())
-
-	coordXY = (coordX,coordY)
-
-	for i in range(len(grid)):
-		for key in grid[i].keys():
-			if key == coordXY:
-				grid[i][coordXY] = case
+	# Modifies grid with the informations given by the player
+	for key in grid.keys():
+		if key == coordXY:
+			grid[key] = case
 
 	return grid
-
